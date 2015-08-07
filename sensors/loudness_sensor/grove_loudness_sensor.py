b9445ebe7a1102bd1129f77671bce30299645b30
Etxahun <etxahun.sanchez@gmail.com>
2015-08-07 13:44:43 +0200

    Loudness sensor
    Loudness sensor

---
+++ sensors/loudness_sensor/grove_loudness_sensor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# GrovePi Example for using the Grove Loudness Sensor (http://www.seeedstudio.com/wiki/Grove_-_Loudness_Sensor)
+#
+# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
+#
+# Have a question about this example?  Ask on the forums here:  http://www.dexterindustries.com/forum/?forum=grovepi
+#
+'''
+## License
+ GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+ Copyright (C) 2015  Dexter Industries
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+'''
+import time
+import grovepi
+
+# Connect the Grove Loudness Sensor to analog port A0
+# SIG,NC,VCC,GND
+loudness_sensor = 0
+
+while True:
+    try:
+        # Read the sound level
+        sensor_value = grovepi.analogRead(loudness_sensor)
+
+        print ("sensor_value =", sensor_value)
+        time.sleep(.5)
+
+    except IOError:
+        print ("Error")
---
+++ sensors/loudness_sensor/grove_sound_sensor.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# GrovePi Example for using the Grove Sound Sensor and the Grove LED
+#
+# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
+#
+# Modules:
+#	 http://www.seeedstudio.com/wiki/Grove_-_Sound_Sensor
+#	 http://www.seeedstudio.com/wiki/Grove_-_LED_Socket_Kit
+#
+# Have a question about this example?  Ask on the forums here:  http://www.dexterindustries.com/forum/?forum=grovepi
+#
+'''
+## License
+ GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+ Copyright (C) 2015  Dexter Industries
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+'''
+
+import time
+import grovepi
+
+# Connect the Grove Sound Sensor to analog port A0
+# SIG,NC,VCC,GND
+sound_sensor = 0
+
+# Connect the Grove LED to digital port D5
+# SIG,NC,VCC,GND
+led = 5
+
+grovepi.pinMode(sound_sensor,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
+
+# The threshold to turn the led on 400.00 * 5 / 1024 = 1.95v
+threshold_value = 400
+
+while True:
+    try:
+        # Read the sound level
+        sensor_value = grovepi.analogRead(sound_sensor)
+
+        # If loud, illuminate LED, otherwise dim
+        if sensor_value > threshold_value:
+            grovepi.digitalWrite(led,1)
+        else:
+            grovepi.digitalWrite(led,0)
+
+        print ("sensor_value =", sensor_value)
+        time.sleep(.5)
+
+    except IOError:
+        print ("Error")
